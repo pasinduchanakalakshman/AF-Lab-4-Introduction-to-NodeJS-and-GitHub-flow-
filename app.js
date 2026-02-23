84d0ed4cd6d9baa5209da0ef790c4b24ac3f1437
Pasindu Chanaka Lakshman <pasinduchanakalakshman@gmail.com>
2026-02-23 17:17:11 +0530

    feat: implement file writing functionality and update file.txt content

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ console.log("Hello World!");
 console.log("Welcome to Node.js development.");
 
 const fs = require('fs'); 
+
+
+// Reading from a file
 fs.readFile('file.txt', 'utf8', function (err, data) { 
 if (err) throw err; 
 console.log(data); 
+}); 
+
+// Writing to a file
+fs.writeFile('file.txt', 'Hello World!', function (err) { 
+  if (err) throw err; 
+  console.log('File saved!'); 
 }); 
