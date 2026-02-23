f33287a9c1e1d1646cf2dd5d775be0452315759e
Pasindu Chanaka Lakshman <pasinduchanakalakshman@gmail.com>
2026-02-23 17:29:56 +0530

    feat: create a web server

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-console.log("Hello World!");
-console.log("Welcome to Node.js development.");
-
+const http = require('http');
 const fs = require('fs'); 
 
 
@@ -15,3 +13,11 @@ fs.writeFile('file.txt', 'Hello World!', function (err) {
   if (err) throw err; 
   console.log('File saved!'); 
 }); 
+
+// Creating a server
+ 
+http.createServer(function (req, res) { 
+  res.writeHead(200, {'Content-Type': 'text/html'}); 
+  res.write('Hello World!'); 
+  res.end(); 
+}).listen(8080);
